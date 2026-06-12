4e29ec1c9f29ec6b20a46de72b178477b2d1084e
judylin <judy159703@gmail.com>
2026-06-12 23:05:17 +0800

    feat: 實作頁面標題列與導覽列組件，整合路由切換與購物車件數通知

--- angular-shopping-cart/src/app/components/navbar/navbar.ts
+++ angular-shopping-cart/src/app/components/navbar/navbar.ts
@@ -1,9 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { CartService } from '../../services/cart.ts';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  standalone: true,
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './navbar.html',
-  styleUrl: './navbar.css',
+  styleUrl: './navbar.css'
 })
-export class Navbar {}
+export class NavbarComponent {
+  private cartService = inject(CartService);
+
+  totalCount = this.cartService.totalItemsCount;
+}
